c6e3671e02a3531f5fd3f75d81bc9a909f1b1f4e
Ivan Garcia <isc.ivgarcia@gmail.com>
2017-10-28 13:22:35 -0500

    Clase 8 - Parte 1 spring tx

--- 3-spring-data-jdbc/src/main/java/org/certificatic/spring/tx/pratica26/springtx/config/TransactionalAnnotationAppConfiguration.java
+++ 3-spring-data-jdbc/src/main/java/org/certificatic/spring/tx/pratica26/springtx/config/TransactionalAnnotationAppConfiguration.java
@@ -2,16 +2,20 @@ package org.certificatic.spring.tx.pratica26.springtx.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configurable
+@Configuration
 // Habilitar Transaction Managemer
+@EnableTransactionManagement(order = 100)
 // Habilitar AspectJ AutoProxy
+@EnableAspectJAutoProxy
 @ComponentScan({ "org.certificatic.spring.tx.pratica26",
 		"org.certificatic.spring.tx.util.bean.api.impl",
 		"org.certificatic.spring.tx.aop" })
@@ -19,8 +23,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class TransactionalAnnotationAppConfiguration {
 
 	@Bean
-	public PlatformTransactionManager transactionManager(
-			DataSource datasource) {
+	public PlatformTransactionManager transactionManager(DataSource datasource) {
 		DataSourceTransactionManager dstm = new DataSourceTransactionManager();
 		dstm.setDataSource(datasource);
 		return dstm;
